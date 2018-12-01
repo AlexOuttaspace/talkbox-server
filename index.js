b0ec099731b6ea10274d2c92ed5b6ea9f9abb0a7
alexander.pomazan <alexander.pomazan@jetruby.com>
2018-12-01 22:01:53 +0300

    feat: create subscription server

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 require('dotenv').config()
 
-const express = require('express'),
-    cors = require('cors'),
-    bodyParser = require('body-parser'),
+const express = require('express')
+const { Server } = require('http')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+const {
+  execute,
+  subscribe,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql')
+const GraphHTTP = require('express-graphql')
+const app = express()
 
-    app = express(),
+const PORT = process.env.PORT || 3020
 
-    PORT = process.env.PORT || 1994
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      message: {
+        type: GraphQLString,
+        resolve() {
+          return 'Hello World!'
+        }
+      }
+    }
+  })
+})
+    
+app.use('/api/ql', GraphHTTP({
+  schema,
+  graphiql: true
+}))
 
-app.use(cors())
+const server = Server(app)
 
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-    res.send('hi there')
+SubscriptionServer.create({
+  schema,
+  execute,
+  subscribe
+}, {
+  server,
+  path: '/api/ws'
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
+server.listen(PORT, console.log(`Server started on port ${PORT}`))
