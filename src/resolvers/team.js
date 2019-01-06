e0dbaff686d1172cd96743bdfa97b15f7982cb8a
alexander.pomazan <alexander.pomazan@jetruby.com>
2019-01-06 06:03:29 +0300

    feat: create general channel for every new team

--- src/resolvers/team.js
+++ src/resolvers/team.js
@@ -5,7 +5,11 @@ export const team = {
   Mutation: {
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        await models.Team.create({ ...args, owner: user.id })
+        const createdTeam = await models.Team.create({ ...args, owner: user.id })
+
+        // not using await here to return response faster
+        models.Channel.create({ name: 'general', public: true, teamId: createdTeam.id })
+
         return {
           ok: true
         }
@@ -20,10 +24,15 @@ export const team = {
   },
   Query: {
     allTeams: requiresAuth.createResolver(async (parent, args, { models, user }) =>
-      await models.Team.findAll({ where: { owner: user.id } }, { raw: true })
+      await models.Team.findAll({
+        where: { owner: user.id }
+      }, {
+        raw: true
+      })
     )
   },
   Team: {
-    channels: ({ id }, args, { models }) => models.Channel.findAll({ teamId: id })
+    channels: ({ id }, args, { models }) =>
+      models.Channel.findAll({ where: { teamId: id } })
   }
 }
