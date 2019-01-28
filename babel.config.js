3da99b804269431c440895655a2014e86ae1e741
alexander.pomazan <alexander.pomazan@jetruby.com>
2019-01-29 00:13:05 +0300

    feat: setup the goddamn babel for test environment

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'plugins': [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    [ '@babel/plugin-proposal-class-properties', { 'loose': false } ],
+    '@babel/plugin-proposal-json-strings',
+    'transform-es2015-modules-commonjs'
+  ]
+}
---
+++ src/resolvers/user.test.js
@@ -0,0 +1,21 @@
+// import axios from 'axios'
+
+describe('user resolvers', () => {
+  test('allUsers', async () => {
+    // const response = await axios.post('http://localhost:3020/graphql', {
+    //   query: `
+    //     query {
+    //       allUsers {
+    //         id
+    //         username
+    //         email
+    //       }
+    //     }
+    //   `
+    // })
+
+    // const { data } = response
+
+    expect(true).toBeTruthy()
+  })
+})
